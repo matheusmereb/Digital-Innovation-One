ee73de04a71669cee60b846d8bd31bad3d5b142d
Matheus Mereb <matheus.mereb@gmail.com>
2021-08-11 10:40:19 -0300

    First JS course finished

---
+++ dio-javascript-intro/js/main.js
@@ -0,0 +1,43 @@
+/* var nome = "Matheus Mereb"
+var n1 = 24
+var n2 = 10
+// console.log(nome + ' tem ' + idade + ' anos.');
+// console.log(idade + idade2);
+var frase = "Japão é o melhor time do mundo"
+console.log(frase.replace("Japão", "Brasil"));
+console.log(frase.toUpperCase());
+console.log(n1 * n2); */
+
+/* var lista = ['maçã', 'pêra', 'laranja']
+lista.pop()
+lista.push('uva')
+console.log(lista[1]);
+console.log(lista.length);
+console.log(lista.reverse());
+console.log(lista.toString());
+console.log(lista.join(' - ')); */
+
+/* var frutas = [{nome: 'maçã', cor: 'vermelha'}, {nome: 'uva', cor: 'roxa'}]
+console.log(frutas[1].nome); */
+
+/* var idade = prompt('Qual a sua idade?')
+// var idade = 18
+if (idade >= 18) {
+    console.log('Maior de idade');
+} else {
+    console.log('Menor de idade');
+} */
+
+/* var count = 0
+while (count <= 5) {
+    console.log(count);
+    count++
+} */
+
+/* var count
+for (count = 1; count <= 5; count++ ){
+    console.log(count);
+} */
+
+var d = new Date()
+console.log(d.getMinutes());
